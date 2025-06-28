1f6b6f9634b7e46ee776ec1e891fd67a6a1bc041
Hashan Munasinghe <Hashan.Nilupul@gmail.com>
2025-06-28 05:28:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/components/shared/back-to-top.tsx
+++ src/components/shared/back-to-top.tsx
@@ -34,10 +34,9 @@ export function BackToTopButton() {
   return (
     <Button
       variant="outline-dark"
-      size="icon"
       onClick={scrollToTop}
       className={cn(
-        'fixed bottom-6 left-6 z-50 shadow-lg transition-opacity duration-300',
+        'fixed bottom-6 left-6 z-50 h-10 w-10 p-0 rounded-tl-[20px] rounded-tr-[8px] rounded-br-[20px] rounded-bl-[20px] transition-opacity duration-300 bg-background',
         isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
       )}
       aria-label="Go to top"
